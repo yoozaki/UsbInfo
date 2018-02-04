9f6745dfa694d485c8ef996f88a9d5cf557bb574
Yoshinori Ozaki <yoozaki@gmail.com>
2018-02-05 07:48:07 +0900

    Minor changes delete unused type and class.

--- UsbInfo/UsbInfo.Natives/DeviceIoControlInvoker.cs
+++ UsbInfo/UsbInfo.Natives/DeviceIoControlInvoker.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Runtime.InteropServices;
 using Microsoft.Win32.SafeHandles;
 
@@ -9,7 +8,7 @@ namespace UsbInfo.Natives
     {
         public static T Invoke<T>(SafeFileHandle handle, int ioControlCode) where T : struct
         {
-            using (var output = new NativeMethods.HGlobal(GetActualSize<T>(handle, ioControlCode)))
+            using (var output = new HGlobal(GetActualSize<T>(handle, ioControlCode)))
             {
                 NativeMethods.ThrowIfSetLastError(NativeMethods.DeviceIoControl(
                     handle, ioControlCode, IntPtr.Zero, 0, output.Value, output.Size, out var _, IntPtr.Zero));
@@ -19,7 +18,7 @@ namespace UsbInfo.Natives
 
         public static T Invoke<T>(SafeFileHandle handle, int ioControlCode, T inputAndOutputStruct) where T : struct
         {
-            using (var inputAndOutputPtr = new NativeMethods.HGlobal<T>(inputAndOutputStruct))
+            using (var inputAndOutputPtr = new HGlobal<T>(inputAndOutputStruct))
             {
                 NativeMethods.ThrowIfSetLastError(NativeMethods.DeviceIoControl(
                     handle, ioControlCode, inputAndOutputPtr.Value, inputAndOutputPtr.Size, 
@@ -28,13 +27,6 @@ namespace UsbInfo.Natives
             }
         }
 
-        private static int GetActualSize<T>(SafeFileHandle handle, int ioControlCode, NativeMethods.HGlobal<T> inputObject) where T : struct
-        {
-            var output = new byte[Marshal.SizeOf<T>()*3];
-            NativeMethods.ThrowIfSetLastError(NativeMethods.DeviceIoControl(
-                handle, ioControlCode, inputObject.Value, inputObject.Size, inputObject.Value, inputObject.Size, out var actualSize, IntPtr.Zero));
-            return actualSize;
-        }
         private static int GetActualSize<T>(SafeFileHandle handle, int ioControlCode) where T : struct
         {
             var output = new byte[Marshal.SizeOf<T>()];
