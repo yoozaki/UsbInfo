86e3b611481056eb11fc787847bceacff9d01fe6
Yoshinori Ozaki <yoozaki@gmail.com>
2018-03-11 17:24:32 +0900

    Add project and license url.

--- UsbInfo/UsbInfo/Properties/AssemblyInfo.cs
+++ UsbInfo/UsbInfo/Properties/AssemblyInfo.cs
@@ -6,9 +6,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("yoozaki")]
 [assembly: AssemblyProduct("UsbInfo")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2018 yoozaki")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+[assembly : AssemblyMetadata("ProjectUrl", "https://github.com/yoozaki/UsbInfo")]
+[assembly : AssemblyMetadata("LicenseUrl", "https://github.com/yoozaki/UsbInfo/blob/master/LICENSE")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("d99a0970-88fa-4ab9-922e-a3e25bab6bb9")]
